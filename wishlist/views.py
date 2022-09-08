0fe21746f523056f3bb0dda9c8811c5b2fc8fd5e
barrysalim <syafribarry@yahoo.com>
2022-09-08 11:35:45 +0700

    sukses

--- wishlist/views.py
+++ wishlist/views.py
@@ -6,6 +6,6 @@ def show_wishlist(request):
     data_barang_wishlist = BarangWishlist.objects.all()
     context = {
     'list_barang': data_barang_wishlist,
-    'nama': 'Om Barry'
+    'nama': 'Syafri Barry Salim'
     }
     return render(request, "wishlist.html", context)
