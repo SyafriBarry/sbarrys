ca1410c68b6070411bed315f230a8ca8632606d8
barrysalim <syafribarry@yahoo.com>
2022-09-15 11:19:43 +0700

    commit 1

--- wishlist/views.py
+++ wishlist/views.py
@@ -6,6 +6,10 @@ def show_wishlist(request):
     data_barang_wishlist = BarangWishlist.objects.all()
     context = {
     'list_barang': data_barang_wishlist,
-    'nama': 'Syafri Barry Salim'
+    'nama': 'Syafri Barry Salim',
+    'npm' : '2106752136'
     }
     return render(request, "wishlist.html", context)
+
+
+    
