8abffe695b95bc1fe5be917f640a40c1d4252d1b
barrysalim <syafribarry@yahoo.com>
2022-09-15 09:50:23 +0700

    melakukan cloning file lab1 pada lab2

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from wishlist.views import show_wishlist
+from wishlist.views import show_xml
 
 app_name = 'wishlist'
+nama_app = 'xml'
 
 urlpatterns = [
     path('', show_wishlist, name='show_wishlist'),
+    path('xml/', show_xml, name='show_xml'),
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,3 +1,5 @@
+from django.http import HttpResponse
+from django.core import serializers
 from django.shortcuts import render
 from wishlist.models import BarangWishlist
 
@@ -6,6 +8,17 @@ def show_wishlist(request):
     data_barang_wishlist = BarangWishlist.objects.all()
     context = {
     'list_barang': data_barang_wishlist,
-    'nama': 'Syafri Barry Salim'
+    'nama': 'Syafri Barry Salim',
+    'npm' : '2106752136'
     }
-    return render(request, "wishlist.html", context)
+    #return render(request, "wishlist.html", context)
+
+def show_xml(request):
+    data = BarangWishlist.objects.all()
+    context = {
+    'list': data,
+    'nama': 'Syafri Barry Salim',
+    'npm' : '2106752136'
+    }
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+    
